723554301bb73ac5a80f721bf6bfb82433158f69
Judit Ross <jtrs@incowia.com>
2016-05-04 10:48:57 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/*
+ * grunt-cubx-webpackage-upload
+ *
+ *
+ * Copyright (c) 2015 Hd Böhlau
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+    // Project configuration.
+    var workspacePath = 'test/webpackages/';
+    var workspaceConfigPath = workspacePath + '.workspace';
+    var activeWebpackage;
+    try {
+        activeWebpackage = grunt.file.readJSON(workspaceConfigPath).activeWebpackage;
+    } catch(err){
+
+    }
+    var manifestWebpackagePath = workspacePath + activeWebpackage + '/manifest.webpackage';
+
+    grunt.initConfig({
+
+        // the option used within the devtools to load the workspace-config
+        workspacePath: workspacePath,
+        workspaceConfigPath: workspaceConfigPath,
+        activeWebpackage: activeWebpackage,
+        manifestWebpackagePath: manifestWebpackagePath
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+
+
+};
---
+++ tasks/choose-active-webpackage.js
@@ -0,0 +1,72 @@
+'use strict';
+var inquirer = require('inquirer');
+var path= require('path');
+var inquirerPath = require.resolve('inquirer');
+var Choices = require( path.join(path.dirname(inquirerPath),'/objects/choices.js'));
+var path = require('path');
+var fs = require('fs');
+module.exports = function(grunt) {
+    grunt.registerTask('_change-activeWebpackage', 'Change the active webpackage.', function() {
+        var workspacePath = grunt.config.get('workspacePath');
+        var activeWebpackage = grunt.config.get('activeWebpackage');
+
+        var workspaceConfigPath =  grunt.config.get('workspaceConfigPath');
+        var configFile;
+        try {
+            var configFile = grunt.file.readJSON(workspaceConfigPath);
+        } catch(err){
+
+        }
+        if (!configFile || typeof configFile !== 'undefined'){
+            configFile = {
+                activeWebpackage: ''
+            }
+        }
+        var done = this.async();
+        fs.readdir(workspacePath, function(err, files) {
+            var choices = [];
+
+            files.forEach(function(file) {
+                var pathName = path.join(workspacePath,file);
+                if (fs.statSync(pathName).isDirectory()) {
+                    if(file === activeWebpackage && choices.length > 0){
+                        choices.splice(0,0,file);
+                    } else {
+                        choices.push(file);
+                    }
+                }
+            });
+
+            choices.push('CANCEL');
+            var options = {
+                questions: [
+                    {
+                        name: 'selectedWebpackage',
+                        type: 'rawlist',
+                        message: 'Please type the index of your choice to change the activeWebpackage ' +
+                        'or to CANCEL: ',
+                        choices: choices,
+                        filter: function(val) {
+                            if (val.indexOf('CANCEL') === 0) {
+                                return 'CANCEL';
+                            }
+                            // return the configKey
+                            return val;
+                        }
+                    }
+                ]
+            };
+            Choices.prototype.setRender = function( render ) {
+                this.renderingMethod = render;
+            };
+            inquirer.prompt(options.questions, function(result) {
+                // do upload, if user confirmed to do so
+                if (result.selectedWebpackage !== 'CANCEL') {
+                    configFile.activeWebpackage = result.selectedWebpackage;
+                    grunt.file.write(workspaceConfigPath,JSON.stringify(configFile,null,4));
+                }
+                done();
+            });
+        });
+    });
+}
